5b2f337b1846147454f434bbdc5678fc6d8336c4
Lovish Sharma <slovish11sharma@gmail.com>
2023-10-18 16:58:54 +0530

    Fixed Search Background

--- README.md
+++ README.md
@@ -47,7 +47,7 @@ These instructions will help you set up the project on your local machine for de
 Now you can access the website locally by opening your web browser and visiting http://localhost:5173/
 
 Site is live at:
-https://lovish141.github.io/InteriorPlantWebsite/
+https://lovish141.github.io/InteriorPlantWebsite/m
 
 
 
--- src/screens/Hero.jsx
+++ src/screens/Hero.jsx
@@ -59,7 +59,7 @@ const Hero = () => {
      </FadeIn>
      <FadeIn delay={0.2} direction="up" padding fullWidth>
       <div className="relative w-full xs:w-[460px] mt-11 ">
-        <input type="text" placeholder="Search" className="rounded-full w-full pl-6 pr-[68px] py-4 bg-primary outline-none text-white text-base xs:text-lg placeholder-white" />
+        <input type="text" placeholder="Search" className="rounded-full w-full pl-6 pr-[68px] py-4 bg-white outline-none text-fontGray text-base xs:text-lg placeholder-fontGray" />
         <img src={searchIcon} alt="" className="absolute top-2/4 -translate-y-2/4 right-3 h-11 w-11 cursor-pointer"></img>
       </div>
      </FadeIn>
